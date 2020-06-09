37ce491e9ba896326fddbf7adac76253bc7b2000
Letícia Araújo <contatoleticiaaraujo@gmail.com>
2020-06-09 14:01:08 -0300

    init commit

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,34 @@
+import { getCustomRepository } from 'typeorm';
+import TransactionRepository from '../repositories/TransactionsRepository';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    category,
+    type,
+    value,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
